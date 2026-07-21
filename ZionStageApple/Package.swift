45b448a29bc350662694d270e77695c6f7f4b1b7
Freedom Labs <freedomlabs@mixer.app>
2026-07-21 08:28:47 -0400

    Add Package.swift and fix project.pbxproj syntax for Xcode and Codemagic

---
+++ ZionStageApple/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "ZionStageApple",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .executable(
+            name: "ZionStageApple",
+            targets: ["ZionStageApple"]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "ZionStageApple",
+            path: "Shared"
+        )
+    ]
+)
